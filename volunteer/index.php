12df225161986c5451d47651836e88bb7f3c9edb
S Rose <rose@publicsphereproject.org>
2025-01-17 10:00:40 -0800

    Added authentication

--- volunteer/index.php
+++ volunteer/index.php
@@ -1,4 +1,13 @@
 <?php
+/* NAME
+ *
+ *  index.php
+ *
+ * CONCEPT
+ *
+ *  Volunteer hub.
+ */
+
 set_include_path(get_include_path() . PATH_SEPARATOR . '../project');
 require '../project/lib/ps.php';
 
@@ -6,11 +15,21 @@ DataStoreConnect();
 Initialize();
 
 if($isLogged = $auth->isLogged()) {
+
+  $userinfo = $auth->getCurrentSessionUserInfo();
+  $user = GetUser($userinfo['uid']);
+  $isVolunteer = GetVolunteer($userinfo['uid']) ? true : false;
+  $isSuper = ($user['role'] == 'super') ? true : false;
+  $isAuthorized = ($isVolunteer || $isSuper) ? true : false;
+
   $action = [
     ' <li><a href="../log.php">Log out</a></li>',
     ' <li><a href="../profile.php">Edit profile</a></li>',
   ];
 } else {
+
+  $isAuthorized = false;
+
   $action = [
     '<li><a href="../log.php">Log in</a></li>',
     '<li><a href="../reset.php">Reset password</a></li>'
@@ -52,6 +71,28 @@ $actions = join('', $action);
 
 <h3>This page is intended to provide resources for developers. It's a draft!</h3>
 
+<?php
+if(!$isLogged) {
+
+  // this user hasn't logged in
+  
+  print "<p>Access to this page is restricted to registered Pattern Sphere
+volunteers. If that decribes you, please <a href=\"../log.php\">log in</a>
+to continue.</p>
+";
+} elseif(!$isAuthorized) {
+
+  // this user logged in, but is neither a volunteer nor a superuser
+
+  print "<p>Access to this page is restricted to registered volunteers. To
+register as a volunteer, please complete this <a href=\"profile.php\">volunteer
+registration form</a>.</p>
+";
+} else {
+
+  // this user is authorized
+
+?>
 <p>If you have any insights or suggestions as to what should be added or 
 clarified, on this page, let us (Roma & Doug) know.</p>
 
@@ -87,36 +128,38 @@ your name taken down), let me know. If you have comments on any of
 these, and there is no name listed, please get in touch with me
 (Doug).</p>
 
-
 <ul>
-<li>Manage and coordinate volunteer development activities 
-<li>Overhaul Front page — https://labs.publicsphereproject.org
+<li>Manage and coordinate volunteer development activities </li>
+<li>Overhaul Front page — https://labs.publicsphereproject.org</li>
 <ul>
-<li>What is it supposed to accomplish? 
+<li>What is it supposed to accomplish? </li>
 For one thing encourage people to go to the PS page (and AM too!) 
-<li>something soon, even if it's just for the short term
+<li>something soon, even if it's just for the short term</li>
 </ul>
-<li>Overhaul PS splash page — https://labs.publicsphereproject.org/ps 
+<li>Overhaul PS splash page — https://labs.publicsphereproject.org/ps</li>
 <ul>
-<li>What is it supposed to accomplish? 
+<li>What is it supposed to accomplish?</li>
 Encourage people to go to the PS page (and AM too!) 
-<li>something soon, even if it's just for the short term
+<li>something soon, even if it's just for the short term</li>
 </ul>
-<li>Master process outline / site map (Tharani (and Sanjana?))
-<li>Finish business rules (processes, roles, rules, patterns, pattern 
+<li>Master process outline / site map (Tharani (and Sanjana?))</li>
+<li>Finish business rules (processes, roles, rules, patterns, pattern </li>
 languages, organizations, users, annotations, workspace, etc.) (Tharani (and Sanjana?))
-<li>Fix (or, even, improve) registration / login process
-<li>Overhaul volunteer page(s) (Roma)
-<li>User Research (Georgia Tech)
-<li>Import Liberating Voices and one or more other pattern languages
-<li>Template development, pattern enhancement and rework (Scott)
-<li>Expert interviews
-<li>Add info about one or Pattern Languages to list
-<li>Work with a community (real or represented) (Lala, Georgia Tech)
+<li>Fix (or, even, improve) registration / login process</li>
+<li>Overhaul volunteer page(s) (Roma)</li>
+<li>User Research (Georgia Tech)</li>
+<li>Import Liberating Voices and one or more other pattern languages</li>
+<li>Template development, pattern enhancement and rework (Scott)</li>
+<li>Expert interviews</li>
+<li>Add info about one or Pattern Languages to list</li>
+<li>Work with a community (real or represented) (Lala, Georgia Tech)</li>
 <ul>
-<li>publish results, help tailor processes to the various communities
+<li>publish results, help tailor processes to the various communities</li>
 </ul>
-<li>develop / curate specialized — and small — pattern languages
+<li>develop / curate specialized — and small — pattern languages</li>
 </ul>
-
-
+<?php
+}
+?>
+</body>
+</html>
